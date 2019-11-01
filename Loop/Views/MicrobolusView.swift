195ae863e62aee1c696de780a958cd6ce6ecf5fb
Ivan Valkou <zloyfey@gmail.com>
2019-11-01 21:35:39 +0300

    New Microboluses Screen

--- Loop/Views/MicrobolusView.swift
+++ Loop/Views/MicrobolusView.swift
@@ -9,92 +9,111 @@
 import SwiftUI
 import Combine
 
-struct DecimalTextField: View {
-    private final class ViewModel: ObservableObject {
-        @Published var text = ""
-        @Binding var value: Double
-
-        private var cancellable: AnyCancellable!
-        private let validCharSet = CharacterSet(charactersIn: "1234567890.,")
-
-        private let valueNumberFormatter: NumberFormatter = {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            formatter.minimumFractionDigits = 0
-            formatter.maximumFractionDigits = 2
-            return formatter
-        }()
-
-        init(value: Binding<Double>) {
-            self._value = value
-            text = valueNumberFormatter.string(from: value.wrappedValue)!
-            cancellable = $text
-                .receive(on: DispatchQueue.main)
-                .sink { val in
-                    if val.rangeOfCharacter(from: self.validCharSet.inverted) == nil {
-                        if let value = self.valueNumberFormatter.number(from: self.text)?.doubleValue {
-                            self.value = value
-                        }
-                    } else {
-                        self.text = String(self.text.unicodeScalars.filter {
-                            self.validCharSet.contains($0)
-                        })
-                    }
-                    if self.text.isEmpty {
-                        self.value = 0
-                    }
-            }
-        }
-    }
-
-    let placeholder: String
-
-    @ObservedObject private var viewModel: ViewModel
-
-    init(placeholder: String, value: Binding<Double>) {
-        self.placeholder = placeholder
-        self.viewModel = ViewModel(value: value)
-    }
-
-    var body: some View {
-        TextField(placeholder, text: $viewModel.text)
-            .keyboardType(.numberPad)
-    }
-}
-
 struct MicrobolusView: View {
-    @State var isMicrobolusesWithCOB = false
-    @State var isMicrobolusesWithoutCOB = false
-    @State var value = 50.0
+    final class ViewModel: ObservableObject {
+        @Published var microbolusesWithCOB: Bool
+        @Published var withCOBValue: Double
+        @Published var microbolusesWithoutCOB: Bool
+        @Published var withoutCOBValue: Double
+
+        @Published fileprivate var pickerWithCOBIndex: Int
+        @Published fileprivate var pickerWithoutCOBIndex: Int
+
+        fileprivate let values = stride(from: 30, to: 301, by: 5).map { $0 }
+
+        private var cancelable: AnyCancellable!
+
+        init(microbolusesWithCOB: Bool, withCOBValue: Double, microbolusesWithoutCOB: Bool, withoutCOBValue: Double) {
+            self.microbolusesWithCOB = microbolusesWithCOB
+            self.withCOBValue = withCOBValue
+            self.microbolusesWithoutCOB = microbolusesWithoutCOB
+            self.withoutCOBValue = withoutCOBValue
+
+            pickerWithCOBIndex = values.firstIndex(of: Int(withCOBValue)) ?? 0
+            pickerWithoutCOBIndex = values.firstIndex(of: Int(withoutCOBValue)) ?? 0
+
+            let withCOBCancelable = $pickerWithCOBIndex
+                .map { Double(self.values[$0]) }
+                .sink { self.withCOBValue = $0 }
+
+            let withoutCOBCancelable = $pickerWithoutCOBIndex
+                .map { Double(self.values[$0]) }
+                .sink { self.withoutCOBValue = $0 }
+
+            cancelable = AnyCancellable {
+                withCOBCancelable.cancel()
+                withoutCOBCancelable.cancel()
+            }
+        }
+
+        func publisher() -> AnyPublisher<(Bool, Double, Bool, Double), Never> {
+            Publishers.CombineLatest4($microbolusesWithCOB, $withCOBValue, $microbolusesWithoutCOB, $withoutCOBValue)
+                .eraseToAnyPublisher()
+        }
+    }
+
+    @ObservedObject var viewModel: ViewModel
+
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
 
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Toggle (isOn: $isMicrobolusesWithCOB) {
-                        Text("Enable with COB")
-                    }
+        Form {
+            Section {
+                HStack {
+                    Image(systemName: "exclamationmark.triangle.fill")
+                        .foregroundColor(.orange)
+                        .padding(.trailing)
 
-                    DecimalTextField(placeholder: "Size", value: $value)
-
-                }
-
-                Section {
-                    Toggle (isOn: $isMicrobolusesWithoutCOB) {
-                        Text("\(value)")
-                    }
-
+                    Text("Caution! Microboluses has the potential to reduce the safety effects of other mitigations, like max temp basal rate. Please be careful!\nThe actual size of the microbolus is always limited to half the recommended bolus.")
+                        .font(.caption)
                 }
 
             }
-            .navigationBarTitle("Microboluses")
-        }
+            Section(footer:
+                Text("This is the maximum minutes of basal that can be delivered as a single Microbolus with uncovered COB. This gives the ability to make Microboluses more aggressive if you choose. It is recommended that the value is set to start at 30, in line with the default, and if you choose to increase this value, do so in no more than 15 minute increments, keeping a close eye on the effects of the changes.")
+            ) {
+                Toggle (isOn: $viewModel.microbolusesWithCOB) {
+                    Text("Enable With Carbs")
+                }
 
+                Picker(selection: $viewModel.pickerWithCOBIndex, label: Text("Maximum Size")) {
+                    ForEach(0 ..< viewModel.values.count) { index in
+                        Text("\(self.viewModel.values[index])").tag(index)
+                    }
+                }
+            }
+
+            if viewModel.microbolusesWithCOB {
+
+                Section(footer:
+                    Text("This is the maximum minutes of basal that can be delivered as a single Microbolus without COB.")
+                ) {
+                    Toggle (isOn: $viewModel.microbolusesWithoutCOB) {
+                        Text("Enable Without Carbs")
+                    }
+                    Picker(selection: $viewModel.pickerWithoutCOBIndex, label: Text("Maximum Size")) {
+                        ForEach(0 ..< viewModel.values.count) { index in
+                            Text("\(self.viewModel.values[index])").tag(index)
+                        }
+                    }
+                }
+            }
+
+        }
+        .navigationBarTitle("Microboluses")
     }
 }
 
 struct MicrobolusView_Previews: PreviewProvider {
     static var previews: some View {
-        MicrobolusView().environment(\.colorScheme, .dark)
+        MicrobolusView(viewModel: .init(
+            microbolusesWithCOB: false,
+            withCOBValue: 30,
+            microbolusesWithoutCOB: false,
+            withoutCOBValue: 30)
+        )
+            .environment(\.colorScheme, .dark)
     }
 }
